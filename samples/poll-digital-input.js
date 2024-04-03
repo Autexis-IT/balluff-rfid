08480c372b7b3c29d07339a5ee5d381f029c316f
she <she@autexis.ch>
2024-04-03 10:48:23 +0200

    initial setup

---
+++ samples/poll-digital-input.js
@@ -0,0 +1,30 @@
+const baluffRfid = require("../lib/index.js");
+
+const conn = baluffRfid.connect({
+    ipAddress: "192.168.10.2",
+    port: 10003,
+
+    onConnect: () => {
+        console.log("connected!");
+
+        const claimedPin = conn.claimPinAsDigitalInput({ pinNumber: 2 });
+        const pollHandle = claimedPin.poll({
+            onRead: ({ error, value }) => {
+                console.log("read digital input", { error, value });
+            },
+            pollIntervalMs: 1000
+        });
+
+        setTimeout(() => {
+            console.log("closing after 10s");
+            pollHandle.stop();
+            claimedPin.release();
+            conn.close();
+        }, 10000);
+    },
+
+    onError: (err) => {
+        console.error("baluff error", err);
+        conn.close();
+    }
+});
---
+++ samples/scan-events.js
@@ -0,0 +1,39 @@
+const baluffRfid = require("../lib/index.js");
+
+const conn = baluffRfid.connect({
+    ipAddress: "192.168.10.2",
+    port: 10003,
+
+    onConnect: () => {
+        console.log("connected!");
+
+        setTimeout(() => {
+            console.log("starting scan...");
+
+            const scanHandle = conn.scanCarriersCumulated({
+                dataType: "E",
+                maxCarriersPerRequest: 999,
+                requestIntervalMs: 10,
+
+                onScan: ({ carriers }) => {
+                    console.log("scan event", carriers);
+                },
+
+                onError: (error) => {
+                    console.error("scan error", error);
+                    clearTimeout(stopTimeoutHandle);
+                }
+            });
+
+            const stopTimeoutHandle = setTimeout(() => {
+                scanHandle.stop();
+                conn.close();
+            }, 2000);
+        }, 1000);
+    },
+
+    onError: (err) => {
+        console.error("baluff error", err);
+        conn.close();
+    }
+});
